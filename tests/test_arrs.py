751b3dbe6b505c39b6c1fff3bfbf5b33e91e18c9
temanbi4 <134223710+temanbi4@users.noreply.github.com>
2023-05-30 16:43:53 +0300

    Update test_arrs.py
    changed 30/05

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,10 +4,22 @@ import pytest
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], -5, "test") == "test"
     with pytest.raises(IndexError):
-        arrs.get([], 0, "test")
+        arrs.gassert 
 
 
 def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+        assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+        assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+        assert arrs.my_slice([], 1) == []
+        assert arrs.my_slice([1, 2, 3], -1) == [3]
+        assert arrs.my_slice([1, 2, 3], 5) == []
+        assert arrs.my_slice([1, 2, 3], 10) == []
+        assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
+        assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
+        assert arrs.my_slice([1, 2, 3], end=-1) == [1, 2]
+        assert arrs.my_slice([1, 2, 3], end=-5) == []
+        assert arrs.my_slice([1, 2, 3], -2, -1) == [2]
+        assert arrs.my_slice([1, 2, 3], end=5) == [1, 2, 3]
+        assert arrs.my_slice([1, 2, 3], end=10) == [1, 2, 3]
